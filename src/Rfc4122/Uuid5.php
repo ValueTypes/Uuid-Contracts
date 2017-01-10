ffb7f69c1f6db45fe1b0df4a54f527451eb5a899
Bas Hamar de la Brethonière <Basje@users.noreply.github.com>
2017-01-10 16:17:13 +0100

    Convert to UTF-8

--- src/Rfc4122/Uuid5.php
+++ src/Rfc4122/Uuid5.php
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * Please read the license at http://www.apache.org/licenses/LICENSE-2.0
  *
- * @copyright (c) 2017 Bas Hamar de la Brethonière
+ * @copyright (c) 2017 Bas Hamar de la BrethoniÃ¨re
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache-2.0
  *
  */
